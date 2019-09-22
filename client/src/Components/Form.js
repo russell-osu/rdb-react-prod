639e1fa8fa699224647cf487b67a8241c51aaef0
russellmoon <russellmoon@gmail.com>
2019-09-22 15:49:08 -0700

    updated index.js and form.js

--- client/src/Components/Form.js
+++ client/src/Components/Form.js
@@ -5,34 +5,20 @@ class Form extends Component {
 		super(props)
 
 		this.initialState = props.entityFields;
-		//this.state = this.initialState;
-		//this.restaurantNames = [];
 
 		this.state = { 
 			fields: this.initialState,
-			restaurantNames: [],
+			restaurantNames: props.restaurantNames,
 			userNames: [],
 			menuItemNames: [],
 			cuisineNames: []
 		};
-	}
 
-	async componentDidMount() {
-		const restNames = await this.fetchRestaurantNames();
-		const usrNames = await this.fetchUserNames();
-		const menuItmNames = await this.fetchMenuItemNames();
-		const cuisNames = await this.fetchCuisineNames();
-		//this.restaurantNames = restNames;
-		//this.setState({restaurantNames: this.restaurantNames});
-		this.setState({restaurantNames: restNames, userNames: usrNames, menuItemNames: menuItmNames, cuisineNames: cuisNames});
 	}
 
 
 	handleChange = event => {
 		const {name, value} = event.target;
-		// this.setState({
-		// 	[name] : value 
-		// });
 
 		this.setState( prevState => ({
 			fields: {
@@ -45,175 +31,40 @@ class Form extends Component {
 
 	submitForm = async () => {
 		const result = await this.props.handleSubmit(this.state.fields);
-		// const restNames = await this.fetchRestaurantNames();
-		// this.restaurantNames = restNames;
+
+		//clear all form fields
 		this.setState( prevState => ({
 			fields: this.initialState
 		}));
+
+
+		const { entity, dropdownFields } = this.props;
+
+		if(entity === "restaurant_review" || entity === "menu_item_review" || entity === "menu_item")
+			document.getElementById("textfield").value = '';
+
+		if(dropdownFields.includes("restaurant_id"))
+			document.getElementById("restaurantDropdown").value = 0;
+
+		if(dropdownFields.includes("user_id"))	
+			document.getElementById("userDropdown").value = 0;
+
+		if(dropdownFields.includes("menu_item_id"))
+			document.getElementById("menuItemDropdown").value = 0;
+
+		if(dropdownFields.includes("cuisine_id"))	
+			document.getElementById("cuisineDropdown").value = 0;
 	}
 
-	fetchRestaurantNames = async () => {
-		const result = await fetch("/restaurant_review/restaurant/names", {
-		  accept: 'application/json',
-		});
-		const result_1 = await result.json();
-		//this.restaurantNames = [...result_1.restaurantNames];
-		return [...result_1.restaurantNames];
-	}
 
-	fetchUserNames = async () => {
-		const result = await fetch("/restaurant_review/user", {
-		  accept: 'application/json',
-		});
-		const result_1 = await result.json();
-		return [...result_1.user];
-	}
-
-	fetchMenuItemNames = async () => {
-		const result = await fetch("/restaurant_review/menu_item/names", {
-		  accept: 'application/json',
-		});
-		const result_1 = await result.json();
-		return [...result_1.menu_item];
-	}
-
-	fetchCuisineNames = async () => {
-		const result = await fetch("/restaurant_review/cuisine", {
-		  accept: 'application/json',
-		});
-		const result_1 = await result.json();
-		return [...result_1.cuisine];
-	}
 
 	render() {
 
-		const { fieldLabels, fieldNames, fieldTypes, dropdownFields, entity } = this.props;
+		const { fieldLabels, fieldNames, fieldTypes, dropdownFields, entity, restaurantNames, userNames, menuItemNames, cuisineNames } = this.props;
 
-		const { restaurantNames, userNames, menuItemNames, cuisineNames } = this.state;
-
-		let textarea = <>  </>;
-		let restaurantDropdown = <>  </>;
-		let restaurantOptions = [];
-		let userDropdown = <>  </>;
-		let userOptions = [];
-		let menuItemDropdown = <>  </>;
-		let menuItemOptions = [];
-		let cuisineDropdown = <>  </>;
-		let cuisineOptions = [];
-
-		if(dropdownFields.includes("restaurant_id")){
-
-			restaurantOptions = restaurantNames.map ( (restaurant, index) => {
-				return (
-					<option value={restaurant.id} >
-						{restaurant.name}
-					</option>
-				);
-			});
-
-			restaurantDropdown = 
-						<>
-							<label>Restaurant</label>
-								<select name="restaurant_id" onChange={this.handleChange}>
-									<option value = "0">
-										[Select a restaurant]
-									</option>
-									{restaurantOptions}
-								</select>
-						</>;
-								
-		}
-
-
-		if(dropdownFields.includes("user_id")){
-
-			userOptions = userNames.map ( (user, index) => {
-				return (
-					<option value={user.id} >
-						{user.fname} {user.lname}
-					</option>
-				);
-			});
-
-			userDropdown = <>
-							<label>User</label>
-								<select name="user_id" onChange={this.handleChange}>
-									<option value = "0">
-										[Select a user]
-									</option>
-									{userOptions}
-								</select>
-							</>;
-		}
-
-
-		if(dropdownFields.includes("menu_item_id")){
-
-			menuItemOptions = menuItemNames.map ( (menuItem, index) => {
-				return (
-					<option value={menuItem.id} >
-						{menuItem.restaurant_name}: {menuItem.menu_item_name}
-					</option>
-				);
-			});
-
-			menuItemDropdown = <>
-							<label>Menu Item</label>
-								<select name="menu_item_id" onChange={this.handleChange}>
-									<option value = "0">
-										[Select a menu item]
-									</option>
-									{menuItemOptions}
-								</select>
-							</>;
-		}
-
-		if(dropdownFields.includes("cuisine_id")){
-
-			cuisineOptions = cuisineNames.map ( (cuisine, index) => {
-				return (
-					<option value={cuisine.id} >
-						{cuisine.name}
-					</option>
-				);
-			});
-
-			cuisineDropdown = <>
-							<label>Cuisine</label>
-								<select name="cuisine_id" onChange={this.handleChange}>
-									<option value = "0">
-										[Select a cuisine]
-									</option>
-									{cuisineOptions}
-								</select>
-							</>;
-		}
-
-
-		if(entity === "restaurant_review" || entity === "menu_item_review") {
-			textarea = 
-					<>
-						<label>Review</label>
-						<textarea name="review" rows="5" cols="80" form="form_post" required onChange={this.handleChange}>
-							Enter your review here.
-						</textarea>
-					</>;
-		}
-
-		if(entity === "menu_item") {
-			textarea = 
-					<>
-						<label>Item Description</label>
-						<textarea name="description" rows="5" cols="80" form="form_post" required onChange={this.handleChange}>
-							Enter the description here.
-						</textarea>
-					</>;
-		}
-
-
-		const formFields = fieldLabels.map( (header, index) => {
+		const fields = fieldLabels.map( (header, index) => {
 			return (
-				<>
+				<div key={header}>
 					<label>{header}</label>
 					<input
 						type = {fieldTypes[index]}
@@ -221,30 +72,231 @@ class Form extends Component {
 						value = {this.state.fields[fieldNames[index]]}
 						onChange={this.handleChange}
 					/>
-				</>
+				</div>
 			);
 
 		});
 
 		return (
-			<form id="form_post">
-				<fieldset>
-					
-					{restaurantDropdown}
-					{cuisineDropdown}
-					{menuItemDropdown}
-					{userDropdown}
-					{formFields}
-					{textarea}
-					<input
-		 				type="button"
-		 				value="Submit"
-		 				onClick={this.submitForm} />
-				</fieldset>
-			</form>
+            <div>   
+                <h3>Entity: {entity}</h3>
+                <form id="form_post">
+                    <fieldset>
+                        
+						<RestaurantDropdown
+							dropdownFields = {dropdownFields}
+							restaurantNames = {restaurantNames}
+							formFields = {this.state.fields}
+							handleChange = {this.handleChange}
+						/>
+						<UserDropdown
+							dropdownFields = {dropdownFields}
+							userNames = {userNames}
+							formFields = {this.state.fields}
+							handleChange = {this.handleChange}
+						/>
+						<MenuItemDropdown
+							dropdownFields = {dropdownFields}
+							menuItemNames = {menuItemNames}
+							formFields = {this.state.fields}
+							handleChange = {this.handleChange}
+						/>
+						<CuisineDropdown
+							dropdownFields = {dropdownFields}
+							cuisineNames = {cuisineNames}
+							formFields = {this.state.fields}
+							handleChange = {this.handleChange}
+						/>						
+
+						{fields}
+						
+						<TextArea
+							entity = {entity}
+							handleChange = {this.handleChange}
+						/>
+                        <input
+                            type="button"
+                            value="Submit"
+                            onClick={this.submitForm} />
+                    </fieldset>
+                </form>
+            </div>
 		);
 
 	}
 }
+
+
+
+
+const RestaurantDropdown =  (props) => {
+
+	let { dropdownFields, handleChange, restaurantNames } = props;
+
+
+	if(!(dropdownFields.includes("restaurant_id"))){
+		return <> </>;
+	}
+
+
+	var restaurantOptions = restaurantNames.map ( (restaurant, index) => {
+
+		return (
+			<option key={restaurant.id} value={restaurant.id} >
+				{restaurant.name}
+			</option>
+		);
+	});
+
+	return (
+			<>
+				<label>Restaurant</label>
+					<select name="restaurant_id" id="restaurantDropdown" onChange={handleChange}>
+						<option value = "0">
+ 							[Select a restaurant]
+ 						</option>
+						{restaurantOptions}
+					</select>
+			</>
+	);	
+	
+}
+
+
+const UserDropdown =  (props) => {
+
+	let { dropdownFields, handleChange, userNames } = props;
+
+
+	if(!(dropdownFields.includes("user_id"))){
+		return <> </>;
+	}
+
+
+	var userOptions = userNames.map ( (user, index) => {
+
+		return (
+			<option key={user.id} value={user.id} >
+				{user.fname} {user.lname}
+			</option>
+		);
+	});
+
+	return (
+			<>
+				<label>User</label>
+					<select name="user_id" id="userDropdown" onChange={handleChange}>
+						<option value = "0">
+ 							[Select a user]
+ 						</option>
+						{userOptions}
+					</select>
+			</>
+	);	
+
+		
+}
+
+
+
+const MenuItemDropdown =  (props) => {
+
+	let { dropdownFields, handleChange, menuItemNames } = props;
+
+	if(!(dropdownFields.includes("menu_item_id"))){
+		return <> </>;
+	}
+
+	var menuItemOptions = menuItemNames.map ( (menuItem, index) => {
+
+		return (
+			<option key={menuItem.id} value={menuItem.id} >
+				{menuItem.restaurant_name}: {menuItem.menu_item_name}
+			</option>
+		);
+	});
+
+	return (
+			<>
+				<label>Menu Item</label>
+					<select name="menu_item_id" id="menuItemDropdown" onChange={handleChange}>
+						<option value = "0">
+ 							[Select a menu item]
+ 						</option>						
+						{menuItemOptions}
+					</select>
+			</>
+	);	
+	
+}
+
+
+
+const CuisineDropdown =  (props) => {
+
+	let { dropdownFields,handleChange, cuisineNames } = props;
+
+
+	if(!(dropdownFields.includes("cuisine_id"))){
+		return <> </>;
+	}
+
+
+	var cuisineOptions = cuisineNames.map ( (cuisine, index) => {
+
+		return (
+			<option key={cuisine.id} value={cuisine.id} >
+				{cuisine.cuisine_name}
+			</option>
+		);
+	});
+
+	return (
+			<>
+				<label>Cuisine</label>
+					<select name="cuisine_id" id="cuisineDropdown" onChange={handleChange}>
+						<option value = "0">
+ 							[Select a cuisine]
+ 						</option>
+						{cuisineOptions}
+					</select>
+			</>
+	);	
+	
+}
+
+
+
+const TextArea = props => {
+
+	let { entity, currRecord, handleChange,  } = props;
+
+	if(entity === "restaurant_review" || entity === "menu_item_review") {
+		return (
+				<>
+					<label>Review</label>
+					<textarea name="review" id="textfield" rows="5" cols="80" form="form_post" required onChange={handleChange}>
+					</textarea>
+				</>
+		);
+	}
+
+	else if(entity === "menu_item") {
+		return (
+				<>
+					<label>Item Description</label>
+					<textarea name="description" id="textfield" rows="5" cols="80" form="form_post" required onChange={handleChange}>
+					</textarea>
+				</>
+		);
+	}
+
+	else
+		return <> </>;
+
+}
+
+
+
 
 export default Form;
